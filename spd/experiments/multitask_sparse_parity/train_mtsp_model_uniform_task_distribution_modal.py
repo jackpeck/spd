9699b1baaf799161ca0455ec893d56383ddeda29
x <>
2026-02-09 15:42:34 -0800

    run over multiple seeds

--- spd/experiments/multitask_sparse_parity/train_mtsp_model_uniform_task_distribution_modal.py
+++ spd/experiments/multitask_sparse_parity/train_mtsp_model_uniform_task_distribution_modal.py
@@ -143,50 +143,51 @@ def train_model(config):
 # print(root_path)
 
 # d_mlp = 64
-for d_mlp in [32, 64, 128, 256, 512]:
-    storer = Storer("/modal_volume/mtsp_results/metrics/")
-    config = TrainConfig(d_mlp=d_mlp)
+# for d_mlp in [32, 64, 128, 256, 512]:
+#     storer = Storer("/modal_volume/mtsp_results/metrics/")
+#     config = TrainConfig(d_mlp=d_mlp)
 
-    storer.add_datestamp_prefix()
-    storer.add_prefix(f"train_mtsp_model_uniform_task_distribution/v10/{config.cache_key()}")
-    # print(storer)
-    torch.manual_seed(config.seed)
-    model = MultitaskSparseParityModel(
-        d_mlp=config.d_mlp, n_control_bits=config.n_control_bits, n_task_bits=config.n_task_bits
-    )
-    dataset = MultitaskSparseParityDataset(
-        n_control_bits=config.n_control_bits,
-        n_task_bits=config.n_task_bits,
-        n_xored_bits=config.n_xored_bits,
-        task_distribution_decay_rate=config.task_distribution_decay_rate,
-        batch_sz=config.batch_sz,
-        size=config.steps,
-    )
-    storer.add_prefix(f"model/{config.steps}")
-    key = "model"
-    assert storer.exists(key), (d_mlp, storer)
-    model.load_state_dict(storer.read(key))
-    losses_by_step_and_task = storer.read("losses_by_step_and_task")
-    torch.manual_seed(0)
-    task_ids, task_bits, parity = dataset.get_batch(batch_sz=1024)
-    logits = model((task_ids, task_bits, ()))
-    val_loss = F.cross_entropy(logits, parity)
-    print(val_loss)
+#     storer.add_datestamp_prefix()
+#     storer.add_prefix(f"train_mtsp_model_uniform_task_distribution/v10/{config.cache_key()}")
+#     # print(storer)
+#     torch.manual_seed(config.seed)
+#     model = MultitaskSparseParityModel(
+#         d_mlp=config.d_mlp, n_control_bits=config.n_control_bits, n_task_bits=config.n_task_bits
+#     )
+#     dataset = MultitaskSparseParityDataset(
+#         n_control_bits=config.n_control_bits,
+#         n_task_bits=config.n_task_bits,
+#         n_xored_bits=config.n_xored_bits,
+#         task_distribution_decay_rate=config.task_distribution_decay_rate,
+#         batch_sz=config.batch_sz,
+#         size=config.steps,
+#     )
+#     storer.add_prefix(f"model/{config.steps}")
+#     key = "model"
+#     assert storer.exists(key), (d_mlp, storer)
+#     model.load_state_dict(storer.read(key))
+#     losses_by_step_and_task = storer.read("losses_by_step_and_task")
+#     torch.manual_seed(0)
+#     task_ids, task_bits, parity = dataset.get_batch(batch_sz=1024)
+#     logits = model((task_ids, task_bits, ()))
+#     val_loss = F.cross_entropy(logits, parity)
+#     print(val_loss)
 
-    losses_by_task = []
-    for i in range(config.n_control_bits):
-        task_ids, task_bits, parity = dataset.get_batch_for_task_ids(
-            batch_sz=1000, task_ids=torch.tensor(i)
-        )
-        logits = model((task_ids, task_bits, ()))
-        loss = F.cross_entropy(logits, parity)
-        losses_by_task.append(loss.item())
-    losses_by_task = torch.tensor(losses_by_task)
-    print((losses_by_task < torch.log(torch.tensor(1.5))).float().mean())
+#     losses_by_task = []
+#     for i in range(config.n_control_bits):
+#         task_ids, task_bits, parity = dataset.get_batch_for_task_ids(
+#             batch_sz=1000, task_ids=torch.tensor(i)
+#         )
+#         logits = model((task_ids, task_bits, ()))
+#         loss = F.cross_entropy(logits, parity)
+#         losses_by_task.append(loss.item())
+#     losses_by_task = torch.tensor(losses_by_task)
+#     print((losses_by_task < torch.log(torch.tensor(1.5))).float().mean())
 
 
 # config = TrainConfig(d_mlp=64)
 # train_model(config)
+import itertools
 
 
 @app.local_entrypoint()
@@ -194,7 +195,12 @@ def main():
     # config = TrainConfig(d_mlp=64)
     # train_model.remote(config)
 
-    configs = [TrainConfig(d_mlp=d_mlp) for d_mlp in [32, 64, 128, 256, 512]]
+    d_mlps = [32, 64, 128, 256, 512]
+    seeds = list(range(5))
+
+    configs = [TrainConfig(d_mlp=d_mlp, seed=seed) for d_mlp in d_mlps for seed in seeds]
+    # print(configs)
+    # configs = [TrainConfig(d_mlp=d_mlp) for d_mlp in ]
     for result in train_model.map(configs):
         pass
 
